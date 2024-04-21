167ed19a63123021a81b1ffc4127c1c91343d93b
MonikaBauer83 <153307926+MonikaBauer83@users.noreply.github.com>
2024-04-21 21:28:10 +0200

    Create a new components for different units(Temperature)

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,30 +1,30 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="WeatherInfo row">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <div className="col-9">
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />{" "}
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-          <li>Humidity: {props.data.humidity} %</li>
-          <li>Wind: {props.data.wind} km/h</li>
-        </ul>
-      </div>
-
-      <div className="WeatherInfo-today col-3">
-        <div className="WeatherInfo-icon">
-          <WeatherIcon code={props.data.icon} size={45} />
+      <div className="WeatherInfo-details row">
+        <div className="WeatherInfo-description col-7">
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
-        <span className="temperature">
-          {Math.round(props.data.temperature)}
-        </span>
-        <span className="unit">°C</span>
+
+        <div className="WeatherInfo-today col-5">
+          <div className="WeatherInfo-icon">
+            <WeatherIcon code={props.data.icon} size={45} />
+          </div>
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
       </div>
     </div>
   );
