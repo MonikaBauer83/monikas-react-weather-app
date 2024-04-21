7722fdd4e049162e636ff552187e9845900411de
MonikaBauer83 <153307926+MonikaBauer83@users.noreply.github.com>
2024-04-21 14:09:52 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/monika-bauer1983/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Monika Bauer
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MonikaBauer83/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="App container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/monika-bauer1983/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Monika Bauer
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MonikaBauer83/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
