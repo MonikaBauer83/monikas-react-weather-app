39e94b697c5683dda201dc7f3bedf11e3ab03f3e
MonikaBauer83 <153307926+MonikaBauer83@users.noreply.github.com>
2024-04-21 13:43:05 +0200

    Added GitHup repo link and LinkedIn-link

--- src/App.js
+++ src/App.js
@@ -3,5 +3,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/monika-bauer1983/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Monika Bauer
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MonikaBauer83/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
