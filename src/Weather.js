e61e664ba238aa4cf5dff667e35a31163e0dcad9
MonikaBauer83 <153307926+MonikaBauer83@users.noreply.github.com>
2024-04-21 14:45:06 +0200

    Added content for current weather

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>Vienna</h1>
+      <ul>
+        <li>Sunday 14:20</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            scr="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 10%</li>
+            <li>Humidity: 70%</li>
+            <li>Wind: 30km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
